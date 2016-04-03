1b0d9ea6d7d261c71dc03c058e2f3993fcb605e8
Pranjal Paliwal <paliwal.pranjal83@gmail.com>
2016-04-03 09:57:17 +0530

    Set the form runnable

--- src/get/it/back/GetItBack.java
+++ src/get/it/back/GetItBack.java
@@ -16,6 +16,11 @@ public class GetItBack {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginScreen().setVisible(true);
+            }
+        });
     }
     
 }
